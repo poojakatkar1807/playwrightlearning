733d2c549f6ee9e7b85acd7a8356909ad685fb3d
poojakatkar1807 <poojakatkar1807@gmail.com>
2025-08-10 10:10:39 +0530

    Create first.js

---
+++ jslearning/first.js
@@ -0,0 +1,24 @@
+
+//console.log("Hello world");
+
+// vriable = vary (change) + able
+//const a = 10;
+//a = 20;
+//console.log(a);
+
+// var and const
+// let 
+
+/*
+if(10 == 10){
+    let x= 10;
+}
+
+console.log(x);
+*/
+
+// Hoisting
+console.log(x);
+let x = 10;
+
+// var, let and const 
