a0d34fcfaa1a204f40d2c15cddaea145561881da
poojakatkar1807 <poojakatkar1807@gmail.com>
2025-08-28 12:34:10 +0530

    Create datatype.js

---
+++ jslearning/datatype.js
@@ -0,0 +1,11 @@
+var student1scores = [10,20,30,40];
+student1scores[3]= 90;
+console.log(student1scores);
+student1scores.push(50);
+console.log(student1scores);
+student1scores.pop();
+console.log(student1scores);
+student1scores.shift();
+console.log(student1scores);
+student1scores.unshift(50)
+console.log(student1scores);
