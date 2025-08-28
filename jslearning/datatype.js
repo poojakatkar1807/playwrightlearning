0b1c8e188ef296d5a899ab9031edd47ce4adc547
poojakatkar1807 <poojakatkar1807@gmail.com>
2025-08-28 16:53:16 +0530

    yes

--- jslearning/datatype.js
+++ jslearning/datatype.js
@@ -1,11 +1,48 @@
-var student1scores = [10,20,30,40];
+/*var student1scores = [10,20,30,40];
+//added 90 at 3rd index
 student1scores[3]= 90;
 console.log(student1scores);
+//added value at last
 student1scores.push(50);
 console.log(student1scores);
+//removing data from last
 student1scores.pop();
 console.log(student1scores);
+//removing data from first
 student1scores.shift();
 console.log(student1scores);
+//adding data at 0th index
 student1scores.unshift(50)
-console.log(student1scores);
+console.log(student1scores)*/
+
+/*var arr = [1,5,9,0,3];
+console.log(arr);
+//inserting value at 1st index
+arr.splice(1,0,7);
+console.log(arr);
+//delete 2 value from 2nd index
+arr.splice(2,2);
+console.log(arr);
+console.log(arr.length);*/
+
+/*var student1 = {
+     "Math" : 90,
+     "Eng"  : 80,
+     "Arts" : 50,
+     "History" : 70,
+}
+var student2 = {
+     "Eng"  : 80,
+     "History" : 70,
+     "Math" : 90,
+     "Gk" : 90,
+     "attendence":[true,false,true]
+}
+//console.log(student2["attendence"][3]);*/
+
+const class12thMarks = require('./data.json');
+console.log(class12thMarks["1"]["Marks"]["Eng"]);
+
+
+
+
